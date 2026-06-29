a528b8221d0756641f25b2b5b9fb613b5aaff682
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-06-29 16:00:32 +0100

    Added methods for boundary conditions and building solvers and solving for 1D heat equation

--- focus/equations/pde1D/heat_equation.py
+++ focus/equations/pde1D/heat_equation.py
@@ -4,10 +4,16 @@ from firedrake.function import Function
 from firedrake.functionspaceimpl import WithGeometry
 from firedrake import Constant
 from firedrake.ufl_expr import TrialFunction, TestFunction
+from firedrake import inner, grad, dx
+from firedrake import LinearVariationalProblem, LinearVariationalSolver
+from firedrake.bcs import DirichletBC
 
 
 class HeatEquationSolver1D(PDESolver1D):
+    """A class to solve the 1D heat equation using the Firedrake."""
+
     def __init__(self, mesh: MeshGeometry, function_space: WithGeometry, kappa: float = 1.0, dt: float = 0.1, ):
+        """Initialize the HeatEquationSolver1D with a mesh, function space, thermal diffusivity, and time step size."""
         if not isinstance(mesh, MeshGeometry):
             raise TypeError("mesh must be an instance of firedrake.mesh.MeshGeometry")
         if not isinstance(function_space, WithGeometry):
@@ -20,37 +26,38 @@ class HeatEquationSolver1D(PDESolver1D):
         self.set_initial_condition()
     
     def set_forcing_function(self, f = Constant(0.0)):
+        """Set the forcing function for the heat equation."""
         self.f = Function(self.V, name="Forcing function")
         self.f.interpolate(f)
     
     def set_initial_condition(self, u0 = Constant(0.0)):
+        """Set the initial condition for the heat equation."""
         self.u0 = Function(self.V, name="Initial condition")
         self.u0.interpolate(u0)
     
+    def set_bcs(self, bcs: list ):
+        """Set the boundary conditions for the heat equation."""
+        self.bcs = [DirichletBC(self.V, bc_value, bc_subdomain+1) for bc_subdomain, bc_value in enumerate(bcs)]
+        
+
     def build_solver(self):
+        """Build the linear variational solver for the heat equation."""
         u = TrialFunction(self.V)
         v = TestFunction(self.V)
-        u_old = Function(self.V, name="Solution at previous time step")
+        self.u_old = Function(self.V, name="Solution at previous time step")
         self.u_new = Function(self.V, name="Solution at new time step")
         self.u_new.interpolate(self.u0)
         
         a = (self.dt*inner(grad(u), grad(v))*self.kappa + inner(u, v))*dx 
-        L = (self.dt*inner(self.f, v))*dx + inner(u_old, v)*dx
-        
-        bc = DirichletBC(self.V, 0.0, "on_boundary")
-        self.solver = LinearVariationalSolver(LinearVariationalProblem(a, L, self.u_new, bcs=bc))
+        L = (self.dt*inner(self.f, v))*dx + inner(self.u_old, v)*dx
+
+        self.solver = LinearVariationalSolver(LinearVariationalProblem(a, L, self.u_new, bcs=self.bcs))
     
     def solve(self):
-        V = FunctionSpace(self.mesh, self.fefamily, self.feorder)
-        u = TrialFunction(V)
-        u_new = Function(V, name="Solution at new time step")
-        u_init = Function(V, name="Initial condition")
-        u_desired = Function(V, name="Desired state")
-        m = Function(V, name="Control")
-        m_list = [Function(V, name=f"Control at time hop {i}") for i in range(window_size)]
-        v = TestFunction(V)
-        u_point_wise_error = Function(V, name="Pointwise error")
-        a = (dt*inner(grad(u), grad(v))*k + inner(u, v))*dx 
-        L = (dt*inner(m, v))*dx + inner(u_old, v)*dx
-        bc = DirichletBC(V, 0.0, "on_boundary")
+        """Solve the heat equation for one time step."""
+        if self.solver is None:
+            raise RuntimeError("Solver has not been built. Call build_solver() before solve().")
+        self.solver.solve()
+        self.u_old.assign(self.u_new)
+
 
