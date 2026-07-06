8fbb5a3e811e7ae2360419c273e4a6e75aead2dc
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-07-06 14:39:54 +0100

    Added a new method for updating initial conditions in reduced functional, and completed methods for time-hopping and time-stepping

--- focus/windowing/fixed_window.py
+++ focus/windowing/fixed_window.py
@@ -2,6 +2,7 @@ from firedrake.function import Function
 from firedrake import Constant
 from pyadjoint import ReducedFunctional
 from firedrake.adjoint import pause_annotation
+
 class FixedWindow():
     def __init__(self, window_size, window_stride, pde_solver):
         self.window_size = window_size
@@ -38,54 +39,79 @@ class FixedWindow():
         return self.window_controls
 
 
-    def run_first_window(self, pde_solver, loss_functional, control):
+    def run_first_window(self, loss_functional, control):
         """"
         Run the first window to set up the Reduced Functional and the Optimizer.
         """
-        pde_solver.u_old.assign(pde_solver.u_new)
+        self.pde_solver.u_new.interpolate(self.pde_solver.u0)
+        self.pde_solver.u_old.assign(self.pde_solver.u_new)
         for m_i in self.window_controls:
             control.interpolate(m_i)
-            pde_solver.solve()
-            pde_solver.u_old.assign(pde_solver.u_new)
+            self.pde_solver.solve()
+            self.pde_solver.u_old.assign(self.pde_solver.u_new)
             # Add the "loss" functional
             self.J += loss_functional()
-        #TODO: Get u_init into the windowing
-        self.Jhat = ReducedFunctional(self.J, controls=self.window_controls, parameters=u_init)
+        self.Jhat = ReducedFunctional(self.J, controls=self.window_controls, parameters=self.pde_solver.u0)
         pause_annotation()
+    
 
-        # J = time_hop_loop(m_list, t_actual, J)
-        # # controls.append(Control(u_init))
-        # # derivative_components = tuple(range(len(m_list)))
-        #     t_current = t_init # Keeps track of the time in a time-hop loop, incremented at each time-hop.
-        # t_window = 0.0 # Keeps track of the time within the current window.
-        # for m_i in controls:
-        #     m.interpolate(m_i)
-        #     solver_heat.solve()
-        #     u_old.assign(u_new)
-        #     t_current += dt
-        #     t_window += dt
-        #     # Add the "loss" functional
-        #     # \int_0^T exp(-0.1*t)*0.5*||u_desired(t) - u(t)||^2 + 0.01*||m||^2 dt        
-        #     J+= loss_functional(t_current, t_window)
-
-        # # Jhat = ReducedFunctional(J, controls = controls, derivative_components=derivative_components)
-        # Jhat = ReducedFunctional(J, controls = controls, parameters=u_init)
-        # pause_annotation()
-        pass
-        
-
-    def time_hop_loop(self, pde_solver, loss_functional, control):
+    def time_hop_loop(self, loss_functional, control):
         """
         Returns a loop over the time hops within the current window.
         """
-        pde_solver.u_old.assign(pde_solver.u_new)
+        self.pde_solver.u_old.assign(self.pde_solver.u_new)
         for m_i in self.window_controls:
             control.interpolate(m_i)
-            pde_solver.solve()
-            pde_solver.u_old.assign(pde_solver.u_new)
+            self.pde_solver.solve()
+            self.pde_solver.u_old.assign(self.pde_solver.u_new)
             # Add the "loss" functional
             self.J += loss_functional()
+
+    def time_step_loop(self, control):
+        """
+        Returns a loop over the time steps within the current window.
+        """
+        for i in range(self.window_stride):
+            control.interpolate(self.window_controls[i])
+            self.pde_solver.solve()
+            self.pde_solver.u_old.assign(self.pde_solver.u_new)
+            self._update_initial_condition()
+            
+
+
+
+        # t_actual = time_step_loop(m_opt[i], t_actual)
+        # u_desired.interpolate(u_desired_expr(t_actual))
+        # u_point_wise_error.interpolate(abs(u_desired - u_new))
+        # if pvdOutput:
+        #     outfile.write(u_new, m, u_desired, u_point_wise_error)
+        # l2_error = norm(u_desired - u_new)
+        # linf_error = max(u_point_wise_error.dat.data)
+        # if verbose:
+        #     PETSc.Sys.Print(f"Time {t_actual:.4f}, L2 error: {l2_error:.6f}, L-infinity error: {linf_error:.6f}")
+        # l2_errors.append(l2_error)
+        # linf_errors.append(linf_error)
+        # u_init.interpolate(u_new)
+
+
+
+
+        # t_current = t_init
+        # m.interpolate(m_opt)
+        # solver_heat.solve()
+        # u_old.assign(u_new)
+        # t_current += dt
+        # return t_current
+    
+    def _update_initial_condition(self):
+        """
+        Set the new initial condition for the PDE solver.
+        """
+        self.pde_solver.u0.interpolate(self.pde_solver.u_new)
+        self.Jhat.update_parameters(self.pde_solver.u0)
         
+
+            
         
 
 
