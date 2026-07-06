5a4e2eb341dc64a572642456fc772d083728e433
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-07-06 12:58:04 +0100

    Base class for loss functionals

--- focus/functionals/base.py
+++ focus/functionals/base.py
@@ -1,14 +1,24 @@
 from abc import ABC, abstractmethod
+from firedrake import inner, dx, exp
 class LossFunctional(ABC):
-    def __init__(self):
+    def __init__(self, weighting):
         pass
 
     @abstractmethod
     def __call__(self, u_desired, u_sol):
+        assemble(self)
         pass
     
     @abstractmethod
-    def cost_control(self, control):
-        pass
-
+    def misfit_loss(self, u_desired, u_sol):
+        return (exp(-lambda_t*t_window)*misfit_weight*inner(u_desired - u_sol, u_desired - u_sol)) 
+        
     
+    @abstractmethod
+    def control_cost(self, control):
+        return inner(control, control)*dx
+
+    def loss_functional(t_current, t_window):
+        u_desired.interpolate(u_desired_expr(t_current))
+        return assemble(((exp(-lambda_t*t_window)*misfit_weight*inner(u_desired - u_new, u_desired - u_new)) + inner(m, m))*dx
+    )
