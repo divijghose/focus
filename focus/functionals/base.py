b6fc3f1bd1f754bf0f726f2e940fa6354a1d2a01
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-07-06 14:49:20 +0100

    Base implementation for loss functionals with a call method

--- focus/functionals/base.py
+++ focus/functionals/base.py
@@ -1,24 +1,20 @@
 from abc import ABC, abstractmethod
-from firedrake import inner, dx, exp
+from firedrake import inner, dx, exp, assemble
 class LossFunctional(ABC):
-    def __init__(self, weighting):
+    def __init__(self, weighting:dict):
+        self.lambda_t = weighting["lambda_t"]
+        self.misfit_weight = weighting["misfit_weight"]
         pass
 
     @abstractmethod
-    def __call__(self, u_desired, u_sol):
-        assemble(self)
-        pass
+    def __call__(self, u_desired, u_sol, t_window, control):
+        assemble(self.control_cost(control) + self.misfit_loss(u_desired, u_sol, t_window))
     
     @abstractmethod
-    def misfit_loss(self, u_desired, u_sol):
-        return (exp(-lambda_t*t_window)*misfit_weight*inner(u_desired - u_sol, u_desired - u_sol)) 
+    def misfit_loss(self, u_desired, u_sol, t_window):
+        return (exp(-self.lambda_t*t_window)*self.misfit_weight*inner(u_desired - u_sol, u_desired - u_sol)) 
         
     
     @abstractmethod
     def control_cost(self, control):
         return inner(control, control)*dx
-
-    def loss_functional(t_current, t_window):
-        u_desired.interpolate(u_desired_expr(t_current))
-        return assemble(((exp(-lambda_t*t_window)*misfit_weight*inner(u_desired - u_new, u_desired - u_new)) + inner(m, m))*dx
-    )
