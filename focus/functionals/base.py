fde7fbe1c6a4c1d884bdf9d1acce59b78920b034
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-07-06 15:22:17 +0100

    Changes to base class of loss functional

--- focus/functionals/base.py
+++ focus/functionals/base.py
@@ -1,20 +1,24 @@
 from abc import ABC, abstractmethod
 from firedrake import inner, dx, exp, assemble
 class LossFunctional(ABC):
-    def __init__(self, weighting:dict):
+    def __init__(self, u_desired, pde_solver, control, weighting: dict):
         self.lambda_t = weighting["lambda_t"]
         self.misfit_weight = weighting["misfit_weight"]
+        self.u_desired = u_desired
+        self.pde_solver = pde_solver
+        self.control = control
+        self.t_window = 0.0
         pass
 
     @abstractmethod
-    def __call__(self, u_desired, u_sol, t_window, control):
-        assemble(self.control_cost(control) + self.misfit_loss(u_desired, u_sol, t_window))
+    def __call__(self):
+        assemble((self.control_cost() + self.misfit_loss()))*dx
     
     @abstractmethod
-    def misfit_loss(self, u_desired, u_sol, t_window):
-        return (exp(-self.lambda_t*t_window)*self.misfit_weight*inner(u_desired - u_sol, u_desired - u_sol)) 
+    def misfit_loss(self):
+        return (exp(-self.lambda_t*self.t_window)*self.misfit_weight*inner(self.u_desired - self.pde_solver.u_new, self.u_desired - self.pde_solver.u_new)) 
         
     
     @abstractmethod
-    def control_cost(self, control):
-        return inner(control, control)*dx
+    def control_cost(self):
+        return inner(self.control, self.control)*dx
