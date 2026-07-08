1b1c0ec63c93fb8e9ae31c7d30c7ca411d939bd4
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-07-08 17:40:12 +0100

    Methods to work with either a single or multiple controls, as well as to updater parameters

--- focus/windowing/fixed_window.py
+++ focus/windowing/fixed_window.py
@@ -41,17 +41,27 @@ class FixedWindow():
         self.current_window_start += self.window_stride
         self.current_window_end += self.window_stride
     
-    def initialize_controls(self, initial_expression=[Constant(0.0)]):
+    def initialize_controls(self, initial_expression=Constant(0.0)):
         """
         Initialize a list of controls for the fixed window.
         """
-        if not isinstance(initial_expression, list):
+        if not isinstance(initial_expression, list) and self.pde_solver.num_controls > 1:
             initial_expression = [initial_expression]
    
-        self.window_controls = [[Function(self.pde_solver.V, name=f"Control{j} at time-hop{i}") for i in range(self.window_size)] for j in range(self.pde_solver.num_controls)]
-        for j, control in enumerate(self.window_controls):
-            for i, m_i in enumerate(control):
-                m_i.interpolate(initial_expression[j])
+        if self.pde_solver.num_controls > 1 and len(initial_expression) != self.pde_solver.num_controls:
+            raise ValueError(f"Length of initial_expression list ({len(initial_expression)}) must match the number of controls ({self.pde_solver.num_controls}).")
+
+        if self.pde_solver.num_controls == 1:
+            self.window_controls = [Function(self.pde_solver.V, name=f"Control at time-hop{i}") for i in range(self.window_size)]
+            for m_i in self.window_controls:
+                m_i.interpolate(initial_expression)
+        else:
+            self.window_controls = [[Function(self.pde_solver.V, name=f"Control{j} at time-hop{i}") for i in range(self.window_size)] for j in range(self.pde_solver.num_controls)]
+
+            for j, control in enumerate(self.window_controls):
+                for i, m_i in enumerate(control):
+                    m_i.interpolate(initial_expression[j])
+        print(f"Initialized {self.pde_solver.num_controls} control variable(s) for the fixed window with size {self.window_size}.")
         return self.window_controls
 
 
@@ -59,32 +69,59 @@ class FixedWindow():
         """"
         Run the first window to set up the Reduced Functional and the Optimizer.
         """
-        for m_i in self.window_controls:
-            control.interpolate(m_i)
+
+        for i in range(self.window_size):
+            if self.pde_solver.num_controls > 1:
+                for j in range(self.pde_solver.num_controls):
+                    control[j].interpolate(self.window_controls[j][i])
+            else:
+                control.interpolate(self.window_controls[i])
             self.pde_solver.solve()
             # Add the "loss" functional
             self.J += loss_functional()
-        self.Jhat = ReducedFunctional(self.J, controls=self.window_controls, parameters=self.pde_solver.u0)
+        self.Jhat = ReducedFunctional(self.J, controls=self.window_controls, parameters=self.pde_solver.p)
         pause_annotation()
-    
+        
+    #FIXME: Should run first window be called separately?
+    #FIXME: The loops below don't advance the window, and don't return the actual time
+    #TODO: Should time_hop_loop and time_step_loop be combined into one function? They are very similar, but the time_hop_loop also computes the loss functional.
 
-    def time_hop_loop(self, loss_functional, control):
+    def _time_hop_loop(self, loss_functional, control):
         """
         Returns a loop over the time hops within the current window.
         """
-        for m_i in self.window_controls:
-            control.interpolate(m_i)
+        for i in range(self.window_size):
+            if self.pde_solver.num_controls > 1:
+                for j in range(self.pde_solver.num_controls):
+                    control[j].interpolate(self.window_controls[j][i])
+            else:
+                control.interpolate(self.window_controls[i])
             self.pde_solver.solve()
             # Add the "loss" functional
             self.J += loss_functional()
 
-    def time_step_loop(self, control):
+    def _time_step_loop(self, control):
         """
         Returns a loop over the time steps within the current window.
         """
         for i in range(self.window_stride):
-            control.interpolate(self.window_controls[i])
+            if self.pde_solver.num_controls > 1:
+                for j in range(self.pde_solver.num_controls):
+                    control[j].interpolate(self.window_controls[j][i])
+            else:
+                control.interpolate(self.window_controls[i])
             self.pde_solver.solve()
+    
+    
+
+    def update_parameters(self):
+        """
+        Update the parameters of the PDE solver for the next window.
+        """
+        self.pde_solver.set_parameters()
+        self.Jhat.update_parameters(self.pde_solver.p)
+    
+
 
         
 
