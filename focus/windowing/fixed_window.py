8c7d1f4b1341760b8715eafc3e187f9a8a9dc228
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-07-08 16:33:05 +0100

    Methods to return time-steps and to admit multiple controls

--- focus/windowing/fixed_window.py
+++ focus/windowing/fixed_window.py
@@ -21,7 +21,19 @@ class FixedWindow():
         Returns the start and end indices of the current window.
         """
         return self.current_window_start, self.current_window_end
-    
+
+    def get_window_start_time(self):
+        """
+        Returns the start time of the current window.
+        """
+        return self.current_window_start * self.pde_solver.dt
+
+    def get_window_end_time(self):
+        """
+        Returns the end time of the current window.
+        """
+        return self.current_window_end * self.pde_solver.dt
+
     def advance_window(self):
         """
         Advances the window by the specified stride.
@@ -29,13 +41,17 @@ class FixedWindow():
         self.current_window_start += self.window_stride
         self.current_window_end += self.window_stride
     
-    def initialize_controls(self, initial_expression=Constant(0.0)):
+    def initialize_controls(self, initial_expression=[Constant(0.0)]):
         """
         Initialize a list of controls for the fixed window.
         """
-        self.window_controls = [Function(self.pde_solver.V, name=f"Control at time-hop{i}") for i in range(self.window_size)]
-        for control in self.window_controls:
-            control.interpolate(initial_expression)
+        if not isinstance(initial_expression, list):
+            initial_expression = [initial_expression]
+   
+        self.window_controls = [[Function(self.pde_solver.V, name=f"Control{j} at time-hop{i}") for i in range(self.window_size)] for j in range(self.pde_solver.num_controls)]
+        for j, control in enumerate(self.window_controls):
+            for i, m_i in enumerate(control):
+                m_i.interpolate(initial_expression[j])
         return self.window_controls
 
 
@@ -46,7 +62,6 @@ class FixedWindow():
         for m_i in self.window_controls:
             control.interpolate(m_i)
             self.pde_solver.solve()
-            self.pde_solver.u_old.assign(self.pde_solver.u_new)
             # Add the "loss" functional
             self.J += loss_functional()
         self.Jhat = ReducedFunctional(self.J, controls=self.window_controls, parameters=self.pde_solver.u0)
@@ -60,7 +75,6 @@ class FixedWindow():
         for m_i in self.window_controls:
             control.interpolate(m_i)
             self.pde_solver.solve()
-            self.pde_solver.u_old.assign(self.pde_solver.u_new)
             # Add the "loss" functional
             self.J += loss_functional()
 
@@ -71,15 +85,7 @@ class FixedWindow():
         for i in range(self.window_stride):
             control.interpolate(self.window_controls[i])
             self.pde_solver.solve()
-            self._update_initial_condition()
-            
-    #FIXME: This method should be handled by the PDE solver, along with parameter assignment
-    def _update_initial_condition(self):
-        """
-        Set the new initial condition for the PDE solver.
-        """
-        self.pde_solver.u0.interpolate(self.pde_solver.u_new)
-        self.Jhat.update_parameters(self.pde_solver.u0)
+
         
 
             
