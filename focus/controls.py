0cae0304537852360fbebfcc711b0f3aa9f2d2b0
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-07-08 16:09:37 +0100

    Refactored controls module

--- focus/controls.py
+++ focus/controls.py
@@ -1,7 +1,16 @@
-def additive_control(pde_solver):
+from firedrake import Function
+def additive_control(function_space, num_controls=1):
     """
     Additive control function for the PDE solver.
     This function returns the forcing term as the control variable.
     """
-    control = pde_solver.f
-    return control
+    control = [Function(function_space, name=f"Additive control variable {i}") for i in range(num_controls)]
+    return control if num_controls > 1 else control[0]
+
+def multiplicative_control(function_space, num_controls=1):
+    """
+    Multiplicative control function for the PDE solver.
+    This function returns the forcing term as the control variable.
+    """
+    control = [Function(function_space, name=f"Multiplicative control variable {i}") for i in range(num_controls)]
+    return control if num_controls > 1 else control[0]
