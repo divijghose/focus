c650146fd22b16ba54f58303e9b4303d63e22bf0
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-07-09 14:42:46 +0100

    Updates to the fixed windowing code

--- focus/windowing/fixed_window.py
+++ focus/windowing/fixed_window.py
@@ -1,26 +1,15 @@
+from .base import Windowing
 from firedrake.function import Function
 from firedrake import Constant
 from pyadjoint import ReducedFunctional
 from firedrake.adjoint import pause_annotation
 
-class FixedWindow():
+class FixedWindow(Windowing):
     def __init__(self, window_size, window_stride, pde_solver):
-        self.window_size = window_size
-        self.window_stride = window_stride
+        super().__init__(window_size, window_stride)
         self.pde_solver = pde_solver
-        assert self.window_size > 0, "Window size must be greater than zero."
-        assert self.window_stride > 0, "Window stride must be greater than zero."
-        assert self.window_stride <= self.window_size, "Window stride must be less than or equal to window size."
-        self.current_window_start = 0
-        self.current_window_end = window_size
         self.functional_value = 0
         self.J = 0
-    
-    def get_current_window(self):
-        """
-        Returns the start and end indices of the current window.
-        """
-        return self.current_window_start, self.current_window_end
 
     def get_window_start_time(self):
         """
@@ -65,7 +54,7 @@ class FixedWindow():
         return self.window_controls
 
 
-    def run_first_window(self, loss_functional, control):
+    def run_first_window(self, loss_functional):
         """"
         Run the first window to set up the Reduced Functional and the Optimizer.
         """
@@ -73,12 +62,12 @@ class FixedWindow():
         for i in range(self.window_size):
             if self.pde_solver.num_controls > 1:
                 for j in range(self.pde_solver.num_controls):
-                    control[j].interpolate(self.window_controls[j][i])
+                    self.pde_solver.control[j].interpolate(self.window_controls[j][i])
             else:
-                control.interpolate(self.window_controls[i])
+                self.pde_solver.control.interpolate(self.window_controls[i])
             self.pde_solver.solve()
             # Add the "loss" functional
-            self.J += loss_functional()
+            self.J += loss_functional() #BUG: Requires arguments to be passed
         self.Jhat = ReducedFunctional(self.J, controls=self.window_controls, parameters=self.pde_solver.p)
         pause_annotation()
         
@@ -86,30 +75,30 @@ class FixedWindow():
     #FIXME: The loops below don't advance the window, and don't return the actual time
     #TODO: Should time_hop_loop and time_step_loop be combined into one function? They are very similar, but the time_hop_loop also computes the loss functional.
 
-    def _time_hop_loop(self, loss_functional, control):
+    def time_hop_loop(self, loss_functional):
         """
         Returns a loop over the time hops within the current window.
         """
         for i in range(self.window_size):
             if self.pde_solver.num_controls > 1:
                 for j in range(self.pde_solver.num_controls):
-                    control[j].interpolate(self.window_controls[j][i])
+                    self.pde_solver.control[j].interpolate(self.window_controls[j][i])
             else:
-                control.interpolate(self.window_controls[i])
+                self.pde_solver.control.interpolate(self.window_controls[i])
             self.pde_solver.solve()
             # Add the "loss" functional
             self.J += loss_functional()
 
-    def _time_step_loop(self, control):
+    def time_step_loop(self):
         """
         Returns a loop over the time steps within the current window.
         """
         for i in range(self.window_stride):
             if self.pde_solver.num_controls > 1:
                 for j in range(self.pde_solver.num_controls):
-                    control[j].interpolate(self.window_controls[j][i])
+                    self.pde_solver.control[j].interpolate(self.window_controls[j][i])
             else:
-                control.interpolate(self.window_controls[i])
+                self.pde_solver.control.interpolate(self.window_controls[i])
             self.pde_solver.solve()
     
     
