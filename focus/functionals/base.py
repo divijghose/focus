6414b7a3f9e84752984f36ea0690d2ed7550d1cb
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-07-09 11:27:52 +0100

    Refactored the base loss class as an abstract class

--- focus/functionals/base.py
+++ focus/functionals/base.py
@@ -1,32 +1,26 @@
 from abc import ABC, abstractmethod
-from firedrake import inner, dx, exp, assemble
-class LossFunctional(ABC):
-    def __init__(self, u_desired, pde_solver, windowing, weighting: dict):
-        self.lambda_t = weighting["lambda_t"]
-        self.control_weight = weighting["control_weight"]
+
+class BaseLoss(ABC):
+    def __init__(self, u_desired, pde_solver, weighting: dict):
+        self.weighting = weighting
         self.u_desired = u_desired
         self.pde_solver = pde_solver
-        self.windowing = windowing
-        self.window_size = windowing.window_size
-        self.window_stride = windowing.window_stride
-   
-    # FIXME: There should be a better way to pass the time information
+ 
     @abstractmethod
-    def __call__(self, control, t_current, t_window):
-        assemble((self.control_cost(control) + self.misfit_loss(t_current, t_window)))*dx
+    def __call__(self):
+        pass
     
     @abstractmethod
-    def misfit_loss(self, t_current, t_window):
+    def misfit_loss(self):
         """
         Returns the misfit loss at time t.
         """
-        u_desired = self.u_desired(t_current)
-        return (exp(-self.lambda_t*t_window)*inner(u_desired - self.pde_solver.u_new, u_desired - self.pde_solver.u_new)) 
+        pass
         
     
     @abstractmethod
-    def control_cost(self, control):
-        return self.control_weight*inner(control, control)*dx
+    def control_cost(self):
+        pass
     
     @abstractmethod
     def get_desired_solution(self, t):
@@ -35,4 +29,3 @@ class LossFunctional(ABC):
         """
         return self.u_desired(t)
 
-#TODO: Rewrite the LossFunctional class to align with the updated solver and windowing classes
