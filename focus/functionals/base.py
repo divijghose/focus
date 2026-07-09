2566f47d14a9474a9bd7c2a9bef2307e5aa1a1fd
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-07-09 11:17:39 +0100

    Intermediate commit for loss functional base class to deal with desired solutions

--- focus/functionals/base.py
+++ focus/functionals/base.py
@@ -1,24 +1,38 @@
 from abc import ABC, abstractmethod
 from firedrake import inner, dx, exp, assemble
 class LossFunctional(ABC):
-    def __init__(self, u_desired, pde_solver, control, weighting: dict):
+    def __init__(self, u_desired, pde_solver, windowing, weighting: dict):
         self.lambda_t = weighting["lambda_t"]
-        self.misfit_weight = weighting["misfit_weight"]
+        self.control_weight = weighting["control_weight"]
         self.u_desired = u_desired
         self.pde_solver = pde_solver
-        self.control = control
-        self.t_window = 0.0
-        pass
-
+        self.windowing = windowing
+        self.window_size = windowing.window_size
+        self.window_stride = windowing.window_stride
+   
+    # FIXME: There should be a better way to pass the time information
     @abstractmethod
-    def __call__(self):
-        assemble((self.control_cost() + self.misfit_loss()))*dx
+    def __call__(self, control, t_current, t_window):
+        assemble((self.control_cost(control) + self.misfit_loss(t_current, t_window)))*dx
     
     @abstractmethod
-    def misfit_loss(self):
-        return (exp(-self.lambda_t*self.t_window)*self.misfit_weight*inner(self.u_desired - self.pde_solver.u_new, self.u_desired - self.pde_solver.u_new)) 
+    def misfit_loss(self, t_current, t_window):
+        """
+        Returns the misfit loss at time t.
+        """
+        u_desired = self.u_desired(t_current)
+        return (exp(-self.lambda_t*t_window)*inner(u_desired - self.pde_solver.u_new, u_desired - self.pde_solver.u_new)) 
         
     
     @abstractmethod
-    def control_cost(self):
-        return inner(self.control, self.control)*dx
+    def control_cost(self, control):
+        return self.control_weight*inner(control, control)*dx
+    
+    @abstractmethod
+    def get_desired_solution(self, t):
+        """
+        Returns the desired solution at time t.
+        """
+        return self.u_desired(t)
+
+#TODO: Rewrite the LossFunctional class to align with the updated solver and windowing classes
