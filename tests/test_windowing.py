80f858a5bd02a695a4c0e1b7a6dd3bdc3180fb3f
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-07-19 17:16:52 +0100

    Working test for fixed windowing with optimization: 1. The bug was in using parameters without initializing them first. This led to a None value of Adj, has to be investigated further

--- tests/test_windowing.py
+++ tests/test_windowing.py
@@ -2,37 +2,45 @@ import pytest
 import numpy as np
 from focus.windowing.fixed_window import FixedWindow
 from firedrake import *
-from firedrake.adjoint import *
-continue_annotation()
-
+# from firedrake.adjoint import *
+from pyadjoint import *
+from pyadjoint.optimization.tao_solver import TAOSolver, MinimizationProblem
+# from firedrake.adjoint import *
 # ============================================================================
 #       Helper classes to build a dummy pde solver  and loss functional to pass to windowing classes
 # ============================================================================
 class DummyPDESolver:
-    def __init__(self):
-
+    def __init__(self, root1=0.0, root2=0.0):
+        
         self.num_controls = 1
-        self.mesh = UnitIntervalMesh(10)
+        self.mesh = UnitIntervalMesh(1)
         self.V = FunctionSpace(self.mesh, "CG", 1)
-        self.u = TrialFunction(self.V)
-        self.v = TestFunction(self.V)
-        self.kappa = Constant(1.0)
-        self.dt = Constant(0.1)
+
+        self.dt = 0.1
         self.u_new = Function(self.V)
-        self.u_old = Function(self.V)
-        self.f = Function(self.V)
-        self.a = self.u * self.v * dx + self.dt * self.kappa * dot(grad(self.u), grad(self.v)) * dx
-        self.L = (self.u_old + self.dt * self.f) * self.v * dx
-        self.solver = LinearVariationalSolver(LinearVariationalProblem(self.a, self.L, self.u_new))
-        self.u_desired = Function(self.V)
+  
+        self.control = Function(self.V)
+        self.control.interpolate(Constant(2.0))
+        self.p = Function(self.V)
+        self.p.interpolate(Constant(0.0))
+        self.root1 = root1
+        self.root2 = root2
+
+    def solve(self):
+        self.u_new = (self.control - self.root1)*(self.control - self.root2) + self.p
 
 class DummyLossFunctional:
-    def __init__(self, control, pde_solver, ):
-        self.lambda_t = 1.0
-        self.control_weight = 1.0
+    def __init__(self,pde_solver):
+
+        self.pde_solver = pde_solver
+
+    def __call__(self, control, t_current, t_window):
+        self.lambda_t = 10.0
+        self.control_weight = 0.001
         self.control_cost = self.control_weight * inner(control, control) * dx
-        self.misfit_loss = inner(pde_solver.u_desired - pde_solver.u_new, pde_solver.u_desired - pde_solver.u_new) * dx
-        self.total_loss = assemble(self.control_cost + self.misfit_loss)
+        self.misfit_loss = self.lambda_t * inner(self.pde_solver.u_new, self.pde_solver.u_new) * dx
+        self.total_loss = assemble(self.misfit_loss + self.control_cost)
+        return self.total_loss
         
 
 
@@ -83,5 +91,65 @@ def test_initialize_controls():
     assert len(controls) == window_size
     for control in controls:
         assert isinstance(control, Function)
+
+def test_run_first_window():
+    continue_annotation()
+    solver = DummyPDESolver()
+    window_size = 3
+    window_stride = 1
+    fixed_window = FixedWindow(window_size=window_size, window_stride=window_stride, pde_solver=solver)
+
+    controls = fixed_window.initialize_controls()
+
+    loss_functional = DummyLossFunctional(solver)
+
+    fixed_window.run_first_window(loss_functional)
+    pause_annotation()
+
+    assert fixed_window.Jhat is not None
+    assert fixed_window.Jhat.derivative() is not None
+
+@pytest.mark.parametrize("root1, root2", [(1.0, 10.0), (2.1, 49.5), (0.0, 56.4)])
+def test_optimization_after_first_window(root1, root2):
+    root1 = root1
+    root2 = root2
+    continue_annotation()
+    solver = DummyPDESolver(root1=root1, root2=root2)
+    window_size = 1
+    window_stride = 1
+    fixed_window = FixedWindow(window_size=window_size, window_stride=window_stride, pde_solver=solver)
+
+    controls = fixed_window.initialize_controls()
+
+    loss_functional = DummyLossFunctional( solver)
+    
+
+    fixed_window.run_first_window(loss_functional)
+    pause_annotation()
+    assert fixed_window.Jhat is not None
+    assert fixed_window.Jhat.derivative() is not None
+    tape = get_working_tape()
+    tape.visualise("tape_test.pdf")
+
+    
+
+
+    problem = MinimizationProblem(fixed_window.Jhat)
+    tao_solver = TAOSolver(problem, {
+    "method": "lbfgs",
+    "max_it": 4,
+    "fatol": 0.0,
+    "frtol": 0.0,
+    "gatol": 1e-9,
+    "grtol": 0.0,
+        
+    })
+    assert fixed_window.Jhat is not None
+    assert fixed_window.Jhat.derivative() is not None
+    optimal_controls = tao_solver.solve()
+    assert optimal_controls is not None
+    assert np.allclose(optimal_controls[0].dat.data, root1), "Optimal control is not as expected."
+    
+
 
 
